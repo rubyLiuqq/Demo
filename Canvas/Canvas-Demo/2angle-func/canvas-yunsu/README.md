e91705bbabeb72c95f4ca79c6659ed5a54d7e9e9
rubyliuqq <1076795115@qq.com>
2018-10-16 00:17:48 +0800

    02 小球匀速运动

---
+++ Canvas/Canvas-Demo/2angle-func/canvas-yunsu/README.md
@@ -0,0 +1,5 @@
+## 小球匀速移动
+
+分别对小球的x, y 方向的位置赋值；
+
+Math.sin(angle) * range 正弦函数，range为振幅。
--- Canvas/Canvas-Demo/2angle-func/canvas-yunsu/ball.js
+++ Canvas/Canvas-Demo/2angle-func/canvas-yunsu/ball.js
@@ -1,30 +1,30 @@
-function Ball(radius,color){
-    if(radius === undefined) {radius = 20;}
-    if(color === undefined){color = '#0000ff';}
-    this.x = 0;
-    this.y = 0;
-    this.vx = 0;
-    this.vy = 0;
-    this.radius = radius;
-    this.color = color;
-    this.rotation = 0;
-    this.scaleX = 1;
-    this.scaleY = 1;
-    this.lineWidth = 1;
+function Ball(radius = 20, color = '#0000ff') {
+  this.x = 0;
+  this.y = 0;
+  this.vx =0;
+  this.vy = 0;
+  this.radius = radius;
+  this.color = color;
+
+  this.rotation = 0;
+  this.scaleX = 1;
+  this.scaleY = 1;
+  this.lineWidth = 1;
 }
 
-Ball.prototype.draw = function (context) {
-    context.save();
-    context.translate(this.x,this.y);   // 平移
-    context.rotate(this.rotation);
-    context.scale(this.scaleX,this.scaleY);
-    context.lineWidth = this.lineWidth;
-    context.fillStyle = this.color;
-    context.strokeStyle = this.color;
-    context.beginPath();
-    context.arc(0,0,this.radius,0,Math.PI*2,false);
-    context.closePath();
-    context.fill();
-    context.stroke();
-    context.restore();
+Ball.prototype.draw = function (ctx) {
+  ctx.save();
+  ctx.translate(this.x, this.y);
+  ctx.rotate(this.rotation);
+  ctx.scale(this.scaleX, this.scaleY);
+  ctx.lineWidth = this.lineWidth;
+  ctx.fillStyle = this.color;
+  ctx.strokeStyle = this.color;
+
+  ctx.beginPath();
+  ctx.arc(0, 0, this.radius, 0, Math.PI * 2, false);
+  ctx.closePath();
+  ctx.fill();
+  ctx.stroke();
+  ctx.restore();
 }
